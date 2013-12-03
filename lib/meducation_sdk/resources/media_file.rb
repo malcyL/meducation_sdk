b3914c2a72c7c82e1734d5b23abfbb8ee9b43132
Jeremy Walker <jez.walker@gmail.com>
2013-12-03 23:54:51 +0000

    Add media file state enum

--- lib/meducation_sdk/resources/media_file.rb
+++ lib/meducation_sdk/resources/media_file.rb
@@ -9,13 +9,20 @@ module MeducationSDK
     def comments
       @comments ||= ItemComment.where(item_id: id, item_type: "MediaFile")
     end
+
+    module State
+      def self.uploaded; 1 end
+      def self.processing; 2 end
+      def self.available; 3 end
+      def self.redacted; 4 end
+    end
   end
 
   class MediaFileMock < MediaFile
     extend Loquor::ResourceMock
 
     self.attributes = {
-      id: 1, 
+      id: 1,
       user_id: 1,
       title: "Abdominal Ultrasound Tutorial"
     }
--- test/resources/media_file_test.rb
+++ test/resources/media_file_test.rb
@@ -10,5 +10,21 @@ module MeducationSDK
       MeducationSDK::User.expects(:find).with(3)
       media_file.user
     end
+
+    def test_uploaded_state_is_correct
+      assert_equal 1, MeducationSDK::MediaFile::State.uploaded
+    end
+
+    def test_processing_state_is_correct
+      assert_equal 2, MeducationSDK::MediaFile::State.processing
+    end
+
+    def test_available_state_is_correct
+      assert_equal 3, MeducationSDK::MediaFile::State.available
+    end
+
+    def test_redacted_state_is_correct
+      assert_equal 4, MeducationSDK::MediaFile::State.redacted
+    end
   end
 end
