27e67d56c5629228b9382ea81afcc4b3eec5dd82
Jeremy Walker <jez.walker@gmail.com>
2013-12-17 15:38:57 +0000

    Add user to premium tutorials

--- lib/meducation_sdk/resources/premium_tutorial.rb
+++ lib/meducation_sdk/resources/premium_tutorial.rb
@@ -1,6 +1,10 @@
 module MeducationSDK
   class PremiumTutorial < Loquor::Resource
     self.path = "/premium_tutorials"
+
+    def user
+      @user ||= User.find(user_id)
+    end
   end
 
   class PremiumTutorialMock < PremiumTutorial
@@ -9,6 +13,7 @@ module MeducationSDK
     self.attributes = {
       id: 1,
       title: "Al's Medicine Tutorial",
+      user_id: 5
     }
   end
 end
--- test/resources/premium_tutorial_test.rb
+++ test/resources/premium_tutorial_test.rb
@@ -4,6 +4,12 @@ require File.expand_path('../../resource_test', __FILE__)
 module MeducationSDK
   class PremiumTutorialTest < ResourceTest
     test_resource(PremiumTutorial, '/premium_tutorials')
+
+    def test_user_calls_sdk
+      tutorial = PremiumTutorial.new(id: 2, user_id: 5)
+      MeducationSDK::User.expects(:find).with(5)
+      tutorial.user
+    end
   end
 end
 
