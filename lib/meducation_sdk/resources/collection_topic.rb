f74b0e9b12b661c277ad0c3f7f6414a980787b3b
Rob Styles <rob.styles@dynamicorange.com>
2014-02-03 14:04:37 +0000

    Added users and comments back in

--- lib/meducation_sdk/resources/collection_topic.rb
+++ lib/meducation_sdk/resources/collection_topic.rb
@@ -4,6 +4,14 @@ module MeducationSDK
 
     self.path = "/collection_topics"
 
+    def user
+      @user ||= User.find(user_id)
+    end
+
+    def comments
+      @comments ||= Comment.where(id: comment_ids)
+    end
+
   end
 
   class CollectionTopicMock < Collection
@@ -15,7 +23,7 @@ module MeducationSDK
       description: 'This is a sample description',
       collection_id: 1,
       section_id: 17,
-      resource_ids: [3,4,5],
+      resource_ids: [3, 4, 5],
       views_in_last_30_days: 1,
       views_in_last_60_days: 2,
       rating: 123
--- test/resources/collection_topic_test.rb
+++ test/resources/collection_topic_test.rb
@@ -4,6 +4,12 @@ require File.expand_path('../../resource_test', __FILE__)
 module MeducationSDK
   class CollectionTopicTest < ResourceTest
     test_resource(CollectionTopic, '/collection_topics')
+
+    def test_comments_calls_sdk
+      topic = CollectionTopic.new(comment_ids: [7, 8, 9])
+      MeducationSDK::Comment.expects(:where).with(id: [7, 8, 9])
+      topic.comments
+    end
   end
 end
 
