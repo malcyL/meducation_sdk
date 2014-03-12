db89a68fe34bb7f29e5432cf23c0850f92000fa3
MalcyL <malcolm@landonsonline.me.uk>
2014-03-12 13:01:22 +0000

    Added item views.

---
+++ test/resources/view_test.rb
@@ -0,0 +1,22 @@
+require_relative '../test_helper'
+require_relative '../resource_test'
+
+module MeducationSDK
+  class ViewTest < ResourceTest
+    test_resource(View, '/view')
+
+    def test_item_calls_sdk
+      view = View.new(user_id: 30660, item_id: 2, item_type: "MediaFile")
+      MeducationSDK::MediaFile.expects(:find).with(2)
+      view.item
+    end
+
+    def test_user_calls_sdk
+      view = View.new(user_id: 30660, item_id: 2, item_type: "MediaFile")
+      MeducationSDK::User.expects(:find).with(30660)
+      view.user
+    end
+  end
+end
+
+
